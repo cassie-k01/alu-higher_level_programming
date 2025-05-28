bf856c3e0049331af9b16a461c7505d428d84656
cassie-k01 <c.kivuye@alustudent.com>
2025-05-28 12:38:30 -0700

    fix

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 function factorial(n) {
-    // Factorial of NaN is 1
+    
     if (isNaN(n)) {
         return 1;
     }
 	    if (n <= 1) {
         return 1;
     }
-    // Recursive case
+    
     return n * factorial(n - 1);
 }
 const firstArg = parseInt(process.argv[2], 10);
--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -3,13 +3,13 @@ const args = process.argv.slice(2).map(arg => parseInt(arg, 10));
 
 if (args.length < 2) {
     console.log(0);
-} else {
-    // Use a Set to store unique integers
+g else {
+    
     const uniqueArgs = new Set(args);
 
-    // Convert the Set back to an array and sort it in descending order
+    
     const sortedUniqueArgs = Array.from(uniqueArgs).sort((a, b) => b - a);
 
-    // Print the second biggest integer or 0 if it doesn't exist
+   
     console.log(sortedUniqueArgs.length > 1 ? sortedUniqueArgs[1] : 0);
 }
