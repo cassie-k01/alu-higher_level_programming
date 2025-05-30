adb895ee572e07d597df3ad9a28efc8a9d3a77b3
cassie-k01 <c.kivuye@alustudent.com>
2025-05-30 13:13:24 -0700

    js objects

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const SquareBase = require('./5-square');
+
+class Square extends SquareBase {
+    constructor(size) {
+        super(size); 
+    }
+
+  
+    charPrint(c) {
+        const char = c || 'X'; 
+        for (let i = 0; i < this.height; i++) {
+            console.log(char.repeat(this.width)); 
+        }
+    }
+}
+
+module.exports = Square;
+
