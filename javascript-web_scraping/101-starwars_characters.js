ac1917df63eba56bae37a5ce7c7626004c2e9711
cassie-k01 <c.kivuye@alustudent.com>
2025-06-06 23:09:48 +0000

    js webscrapping

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -3,17 +3,18 @@ const request = require('request');
 const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 request(apiUrl, function (error, response, body) {
-  if (error) {
-    console.error('Error fetching data:', error);
-    return;
-  }
+  if (error) return console.error('Error fetching data:', error);
   const characters = JSON.parse(body).characters;
-  characters.forEach(characterUrl => {
+  const characterNames = [];
+  let completedRequests = 0;
+  if (characters.length === 0) return console.log('Characters not found');
+  characters.forEach((characterUrl, index) => {
     request(characterUrl, function (error, response, body) {
-      if (error) {
-        console.error('Error fetching character data:', error);
-      } else {
-        console.log(JSON.parse(body).name);
+      if (error) return console.error('Error fetching character data:', error);
+      characterNames[index] = JSON.parse(body).name;
+      completedRequests++;
+      if (completedRequests === characters.length) {
+        characterNames.forEach(name => console.log(name));
       }
     });
   });
