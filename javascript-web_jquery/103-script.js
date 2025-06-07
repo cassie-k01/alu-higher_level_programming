31a712c6e3ef199bd995c86fc948ff285855f770
cassie-k01 <c.kivuye@alustudent.com>
2025-06-07 16:26:04 +0000

    webquery

---
+++ javascript-web_jquery/103-script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  function fetchHello() {
+    const langCode = $("#language_code").val();
+    const url = `https://www.fourtonfish.com/hellosalut/hello/?lang=${langCode}`;
+
+    $.get(url, function (data) {
+      $("#hello").text(data.hello);
+    });
+  }
+
+  $("#btn_translate").click(fetchHello);
+
+  $("#language_code").keypress(function (event) {
+    if (event.key === "Enter") {
+      fetchHello();
+    }
+  });
+});
